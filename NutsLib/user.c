fe19c607e673c87c14ea8e2ae08bd3f6c2dea523
HUANsic <wu-zh20@mails.tsinghua.edu.cn>
2026-04-10 15:18:23 +0800

    disable IO pins on sleep; tested on F103

--- NutsLib/user.c
+++ NutsLib/user.c
@@ -27,9 +27,40 @@ NutStatus_e Echo(uint8_t *received_data_ptr, uint32_t received_data_length, uint
 }
 
 /* Enter sleep mode on demand */
+
+static void set_all_pins_pullup(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = {
+			0 };
+	HAL_GPIO_DeInit(NUT_LED_PORT, NUT_LED_PIN);
+	HAL_GPIO_DeInit(NUT_IO_USER_PORT, NUT_IO_USER_PIN);
+	HAL_GPIO_DeInit(NUT_IO1_PORT, NUT_IO1_PIN);
+	HAL_GPIO_DeInit(NUT_IO2_PORT, NUT_IO2_PIN);
+	HAL_GPIO_DeInit(NUT_IO3_PORT, NUT_IO3_PIN);
+	HAL_GPIO_DeInit(NUT_SPI_CS_PORT, NUT_SPI_CS_PIN);
+
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Pin = NUT_LED_PIN;
+	HAL_GPIO_Init(NUT_LED_PORT, &GPIO_InitStruct);
+	GPIO_InitStruct.Pin = NUT_IO_USER_PIN;
+	HAL_GPIO_Init(NUT_IO_USER_PORT, &GPIO_InitStruct);
+	GPIO_InitStruct.Pin = NUT_IO1_PIN;
+	HAL_GPIO_Init(NUT_IO1_PORT, &GPIO_InitStruct);
+	GPIO_InitStruct.Pin = NUT_IO2_PIN;
+	HAL_GPIO_Init(NUT_IO2_PORT, &GPIO_InitStruct);
+	GPIO_InitStruct.Pin = NUT_IO3_PIN;
+	HAL_GPIO_Init(NUT_IO3_PORT, &GPIO_InitStruct);
+	GPIO_InitStruct.Pin = NUT_SPI_CS_PIN;
+	HAL_GPIO_Init(NUT_SPI_CS_PORT, &GPIO_InitStruct);
+}
+
 NutStatus_e EnterSleep_NoWake_MainReg(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
 		uint32_t result_buffer_MAX_size) {
 	*result_length = 0;
+	Nut_LED(1);
+	HAL_Delay(100);
+	Nut_LED(0);
+	set_all_pins_pullup();
 	HAL_SuspendTick();	// disable SysTick
 	HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);	// enter sleep mode
 	return NUT_OK;
@@ -38,6 +69,10 @@ NutStatus_e EnterSleep_NoWake_MainReg(uint8_t *received_data_ptr, uint32_t recei
 NutStatus_e EnterSleep_NoWake_LPReg(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
 		uint32_t result_buffer_MAX_size) {
 	*result_length = 0;
+	Nut_LED(1);
+	HAL_Delay(1000);
+	Nut_LED(0);
+	set_all_pins_pullup();
 	HAL_SuspendTick();	// disable SysTick
 	HAL_PWR_EnterSLEEPMode(PWR_LOWPOWERREGULATOR_ON, PWR_SLEEPENTRY_WFI);	// enter sleep mode
 	return NUT_OK;
@@ -46,6 +81,18 @@ NutStatus_e EnterSleep_NoWake_LPReg(uint8_t *received_data_ptr, uint32_t receive
 NutStatus_e EnterStop_NoWake_MainReg(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
 		uint32_t result_buffer_MAX_size) {
 	*result_length = 0;
+	Nut_LED(1);
+	HAL_Delay(100);
+	Nut_LED(0);
+	HAL_Delay(100);
+	Nut_LED(1);
+	HAL_Delay(100);
+	Nut_LED(0);
+	HAL_Delay(100);
+	Nut_LED(1);
+	HAL_Delay(100);
+	Nut_LED(0);
+	set_all_pins_pullup();
 	HAL_SuspendTick();	// disable SysTick
 	HAL_PWR_EnterSTOPMode(PWR_MAINREGULATOR_ON, PWR_STOPENTRY_WFI);	// enter stop mode
 	return NUT_OK;
@@ -54,6 +101,18 @@ NutStatus_e EnterStop_NoWake_MainReg(uint8_t *received_data_ptr, uint32_t receiv
 NutStatus_e EnterStop_NoWake_LPReg(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
 		uint32_t result_buffer_MAX_size) {
 	*result_length = 0;
+	Nut_LED(1);
+	HAL_Delay(1000);
+	Nut_LED(0);
+	HAL_Delay(1000);
+	Nut_LED(1);
+	HAL_Delay(1000);
+	Nut_LED(0);
+	HAL_Delay(1000);
+	Nut_LED(1);
+	HAL_Delay(1000);
+	Nut_LED(0);
+	set_all_pins_pullup();
 	HAL_SuspendTick();	// disable SysTick
 	HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);	// enter stop mode
 	return NUT_OK;
@@ -62,6 +121,18 @@ NutStatus_e EnterStop_NoWake_LPReg(uint8_t *received_data_ptr, uint32_t received
 NutStatus_e EnterStandby_NoWake(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
 		uint32_t result_buffer_MAX_size) {
 	*result_length = 0;
+	Nut_LED(1);
+	HAL_Delay(100);
+	Nut_LED(0);
+	HAL_Delay(1000);
+	Nut_LED(1);
+	HAL_Delay(100);
+	Nut_LED(0);
+	HAL_Delay(1000);
+	Nut_LED(1);
+	HAL_Delay(100);
+	Nut_LED(0);
+	set_all_pins_pullup();
 	__HAL_PWR_CLEAR_FLAG(PWR_FLAG_WU);		// clear wake-up flag
 	HAL_SuspendTick();	// disable SysTick
 	HAL_PWR_EnterSTANDBYMode();	// enter standby mode
